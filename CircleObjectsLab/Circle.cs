0ed062c6e9135960094b15c2b22ed3092c4b617a
Dan Schuler <104746354+dandanthesoftwareman@users.noreply.github.com>
2022-07-05 21:05:40 -0400

    First Commit

---
+++ CircleObjectsLab/Circle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleObjectsLab
+{
+    internal class Circle
+    {
+        //properties
+        private double Radius;
+        public Circle(double _radius)
+        {
+           Radius = _radius;
+        }
+        public double CalculateCircumference()
+        {
+            return 2 * Math.PI * Radius;
+        }
+        public double CalculateFormattedCircumference()
+        {
+            return FormatNumber(2 * Math.PI * Radius);
+        }
+        public double CalculateFormattedArea()
+        {
+            return FormatNumber(CalculateArea());
+        }
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+        private double FormatNumber(double x)
+        {
+            return Math.Round(x, 2);
+        }
+    }
+}
---
+++ CircleObjectsLab/Program.cs
@@ -0,0 +1,48 @@
+﻿using CircleObjectsLab;
+bool runProgram = true;
+int CircleObjectCount = 0;
+
+Console.WriteLine("Welcome to the circle tester");
+
+while(runProgram)
+{
+    while (true)
+    {
+        try
+        {
+            Console.WriteLine("Enter a radius:");
+            Circle myCircle = new Circle(double.Parse(Console.ReadLine()));
+            Console.WriteLine($"Circumfrence: {myCircle.CalculateFormattedCircumference()}");
+            Console.WriteLine($"Area: {myCircle.CalculateFormattedArea()}");
+            CircleObjectCount++;
+            break;
+        }
+
+        catch (FormatException)
+        {
+            Console.WriteLine("Enter a number for the radius please");
+            continue;
+        }
+
+    }
+    Console.WriteLine("Continue? y/n");
+    while (true)
+    {
+        string response = Console.ReadLine().ToLower().Trim();
+        if (response == "y")
+        {
+            break;
+        }
+        else if (response == "n")
+        {
+            Console.WriteLine($"Goodbye. You created {CircleObjectCount} Circle object(s).");
+            runProgram = false;
+            break;
+        } 
+        else
+        {
+            continue;
+        }
+    }
+}
+
